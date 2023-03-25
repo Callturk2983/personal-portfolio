232bf69db94999bc470800263917f0f5e250c507
Kevin Taylor <kevintaylor&84@gmail.com>
2023-03-25 12:09:35 -0400

    finished home styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Kevin</h2>
+        <div className="prompt">
+          <p>
+            A retired Marine veteran who has always had a passion Tech industry. And is now a Software Engineer turning his dreams in realities by learning and creating projects.
+            <LinkedInIcon />
+            <GitHubIcon />
+            <EmailIcon />
+          </p>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, NPM, Material UI, TailwindCSS,
+              StyleComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS,SQL, Sequelize, PostgreSql, API</span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, TypeScript</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
